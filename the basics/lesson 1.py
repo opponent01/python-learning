f577b79aa368ef0173874239d6ae45664d3da2ec
Opponent01 <mohotok2017@gmail.com>
2025-12-28 23:30:45 +0330

    some learning practice

---
+++ the basics/lesson 1.py
@@ -0,0 +1,28 @@
+weapon= "iron balls"
+affinity= "heavy"
+level= 120
+
+the_build= f"the best build in elden ring is {weapon} with {affinity} affinity at level {level}"
+
+print(the_build)
+print(weapon[::-2])
+
+print(weapon.upper())
+print(weapon.strip(""))
+print(weapon.replace("iron", "heavy"))
+print(weapon.split("n"))
+
+jointhis=("hello", "world")
+
+i_did= " how ".join(jointhis)
+
+print(" what ".join(jointhis))
+
+print(weapon.startswith("iron"))
+
+print(weapon.find("what"))
+print(weapon.count("f"))
+print(weapon.capitalize())
+print(weapon.isupper())
+print(weapon.title())
+print(weapon.strip(",s"))
