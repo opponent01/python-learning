4b989a85c0289b2c1218a43b3e02d50980e07113
Opponent01 <mohotok2017@gmail.com>
2026-02-04 23:14:47 +0330

    Build a Report Card Printer

---
+++ the basics/lessons/lesson 04-02-2026.py
@@ -0,0 +1,15 @@
+#Build a Report Card Printer
+def report_card_printer():
+    is_student= True
+    name= "Alice"
+    age= 20
+    score= 80.5
+    print(name, type(name))
+    print(is_student, type(is_student))
+    print(age, type(age))
+    print(isinstance(score, float))
+    print(score, type(score))
+
+
+
+
