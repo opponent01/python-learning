2d07d0ac6679f0dda194aeac0fea5bedbdebd2f5
Opponent01 <mohotok2017@gmail.com>
2026-02-21 19:25:53 +0330

    Build a Pin Extractor

--- the basics/practice/random practice.py
+++ the basics/practice/random practice.py
@@ -1,17 +1,3 @@
-numbers2= [1,2,3,4,5,6]
-result= []
-for num in numbers2:
-    if num % 2 == 0:
-        result.append(f"{num} even")
-    else:
-        result.append(f"{num} odd")
-print(result)
-
-
-
-
-
-
-
-
+hi= [("hi"),("why")]
+print(hi.count())
 
